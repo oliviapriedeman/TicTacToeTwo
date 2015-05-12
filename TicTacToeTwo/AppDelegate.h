e498364ae3bfa25910aa4fdc96da69124c49c81f
oliviapriedeman <oapriedeman15@TBSLTMBA659.local>
2015-05-11 20:27:47 -0500

    Initial Commit

---
+++ TicTacToeTwo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToeTwo
+//
+//  Created by oapriedeman15 on 5/11/15.
+//  Copyright (c) 2015 oapriedeman15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToeTwo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToeTwo
+//
+//  Created by oapriedeman15 on 5/11/15.
+//  Copyright (c) 2015 oapriedeman15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
